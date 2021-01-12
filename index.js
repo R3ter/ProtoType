e7bb534cca185669c3634f5aeb16ca60d9098dc0
waleed <waleed.sukhon77@gmail.com>
2021-01-12 15:43:10 +0200

    xxxyyzzz

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const server = new ApolloServer({
 
 const app = express();
 server.applyMiddleware({ app });
-app.listen({ port: 4000 }, () =>{
+app.listen({ port: process.env.PORT||4000 }, () =>{
   console.log(`Server ready at http://localhost:4000${server.graphqlPath}`)
 }
 );
