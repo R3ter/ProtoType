846fed4aee98ff8f45635b596e446e9efb3f6775
waleed.sukhon77@gmail.com <waleed.sukhon77@gmail.com>
2021-02-13 19:53:07 +0200

    sad

--- Resolvers/types/WorkingHour.js
+++ Resolvers/types/WorkingHour.js
@@ -1,16 +1,17 @@
 import { checkToken } from "../../methods/Tokens.js"
 const teacherSchedule={
-    async state(parent, args, {req,prisma}){
-        console.log(parent)
-    const {id} = checkToken({token:req.headers.token})
-    const appointment=await prisma.appointment.findFirst({
-        where:{
-            userId:id
-        }
-      })
-      if(appointment)
-        return 2
-    return parent.state>1?1:parent.state
-    }
+    // async state(parent, {date}, {req,prisma}){
+    //     console.log(parent)
+    // const {id} = checkToken({token:req.headers.token})
+    // const appointment=await prisma.appointment.findFirst({
+    //     where:{
+    //         date,
+    //         userId:id
+    //     }
+    //   })
+    //   if(appointment)
+    //     return 2
+    // return parent.state>1?1:parent.state
+    // }
 }
 export default teacherSchedule
