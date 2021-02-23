8c7030af406fca1fd7c4c5fad0b7b4c35d3f8e94
waleed.sukhon77@gmail.com <waleed.sukhon77@gmail.com>
2021-02-23 20:12:26 +0200

    dsds

--- Resolvers/query/getTeacherAppointment.js
+++ Resolvers/query/getTeacherAppointment.js
@@ -2,9 +2,7 @@ import { checkToken } from "../../methods/Tokens.js"
 import moment from 'moment'
 
 const checkTime = (times, hours, minutes, appointments, id) => {
-
   const data = []
-
   times.forEach((e) => {
     const from = e.split("/-/")[0]
     const to = e.split("/-/")[1]
@@ -12,40 +10,46 @@ const checkTime = (times, hours, minutes, appointments, id) => {
     const From = moment(from)
     const To = moment(to)
     var i = moment(From)
-    console.log(moment(i))
-    console.log(To)
-    while (moment(i).isBefore(To)) {
-      if (moment(i).add(hours, "hours").format("HH:mm a")
-        <= To.format("HH:mm a")) {
+    let appointmentCount=0;
+
+    while (moment(i).isBefore(To)&&
+      moment(i).add(hours, "hours").add(minutes, "minutes").format("HH:mm a")
+      <= To.format("HH:mm a")) {
         let state = 0;
         let skip = false
-        appointments.forEach((e) => {
-          if (e.from == moment(i).format("HH:mm a") &&
-            e.to == moment(i).add(hours, "hours")
-              .add(minutes, "minutes").format("HH:mm a")) {
-            state = 1
-          }
-          else if (moment(i).isBetween(moment(e.from),
-                    moment(e.to)) ||
-                    moment(i).add(hours, "hours").add(minutes, "minutes").isBetween(
-                      moment(e.from),
-                      moment(e.to)))
-                      {
-                        i = moment(e.to)
-                        skip = true
-                      }
-                    })
-        if (!skip) {
+        // console.log(moment(i).format("HH:mm a"),
+        // moment(appointments[appointmentCount].from).format("HH:mm a"))
+        
+        if (appointments[appointmentCount]&&
+          moment(appointments[appointmentCount].from).format("HH:mm a")
+           == moment(i).format("HH:mm a") &&
+           moment(appointments[appointmentCount].to).format("HH:mm a")
+            == moment(i).add(hours, "hours")
+        .add(minutes, "minutes").format("HH:mm a")) {
+          state = 1
+        }
+        else if (appointments[appointmentCount]&&(
+          moment(i).isBetween(moment(appointments[appointmentCount].from),
+          moment(appointments[appointmentCount].to)) ||
+          moment(i).add(hours, "hours").add(minutes, "minutes").isBetween(
+            moment(appointments[appointmentCount].from),
+            moment(appointments[appointmentCount].to))))
+            {
+              i = moment(appointments[appointmentCount].to)
+              appointmentCount++;
+              skip = true
+            }
+            if (!skip) {
           data.push({
             from: moment(i).format("HH:mm a"),
             to: moment(i).add(hours, "hours").add(minutes, "minutes").format("HH:mm a"),
             state
           })
+          i = moment(i).add(hours, "hours").add(minutes, "minutes")
         }
       }
-      i = moment(i).add(hours, "hours").add(minutes, "minutes")
     }
-  })
+  )
   return data
 }
 
