43e68b82ec9b746bd51b3d3717eaf1b7f5bf799a
waleed.sukhon77@gmail.com <waleed.sukhon77@gmail.com>
2021-02-26 20:28:40 +0200

    gg

--- Resolvers/query/teacher/getMaterialsForRegister.js
+++ Resolvers/query/teacher/getMaterialsForRegister.js
@@ -1,6 +1,7 @@
 import {checkToken} from './../../../methods/Tokens.js'
 const getMyEducationLevels=async( parent,{educationLevelId},{prisma,req})=>{
     const {id} = checkToken({Roles:"TEACHER",token:req.headers.token})
+    console.log(id)
     return await prisma.materials.findMany({
         where:{
             AND:{
