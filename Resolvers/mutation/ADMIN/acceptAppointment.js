4268f0326c4c43484e7b7df0fe0ca6c24e3a8f6c
waleed <waleed.sukhon77@gmail.com>
2021-03-08 08:15:59 +0200

    wdasd

--- Resolvers/mutation/ADMIN/acceptAppointment.js
+++ Resolvers/mutation/ADMIN/acceptAppointment.js
@@ -2,6 +2,7 @@ import { checkToken } from "../../../methods/Tokens.js"
 
 const teacherAcceptAppointment=async(parent,{AppointmentID},{prisma,req})=>{
     const {id}=checkToken({token:req.headers.token,Roles:["ADMIN"]})
+    if(AppointmentID=="") return false
     const appointment=await prisma.appointment.findUnique({
         where:{
             id:AppointmentID
--- Resolvers/mutation/ADMIN/rejectAdminAppoitment.js
+++ Resolvers/mutation/ADMIN/rejectAdminAppoitment.js
@@ -2,6 +2,8 @@ import { checkToken } from "../../../methods/Tokens.js"
 
 const reject = async(parent,{rejectionReason,AppointmentID},{req,prisma})=>{
     const {id}=checkToken({token:req.headers.token,Roles:["ADMIN"]})
+    if(AppointmentID=="") return false
+
     return !!await prisma.appointment.update({
         where:{
             id:AppointmentID
