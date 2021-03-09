3f5e7b41a3c76df3aba177e1fb3f28b97ad46a3f
waleed <waleed.sukhon77@gmail.com>
2021-03-10 01:34:50 +0200

    sad

--- Resolvers/types/TeacherMapInfo.js
+++ Resolvers/types/TeacherMapInfo.js
@@ -3,9 +3,10 @@ import { checkToken } from "../../methods/Tokens.js"
 const getTeachersOnMap={
     async canContact(parent,args,{prisma,req}){
         const {id} = checkToken({token:req.headers.token})
+        console.log(parent)
         return await prisma.appointment.count({
             where:{
-                teacherId:parent.user.id,
+                teacherId:parent.teacherId,
                 studentId:id,
                 stateKey:"accepted"
             }
