72a9e073454a45078091e18563a4b62deb3402a9
waleed <waleed.sukhon77@gmail.com>
2021-04-12 19:17:22 +0300

    teachers count

--- Resolvers/types/Materials.js
+++ Resolvers/types/Materials.js
@@ -20,9 +20,9 @@ const Materials={
                     some:{
                         id:parent.id
                     },
-                    TeacherProfile:{
-                        teacherIsActive:true
-                    }
+                },
+                teacherProfile:{
+                    teacherIsActive:true
                 }
             },
             count:true
