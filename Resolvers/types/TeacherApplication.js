2bd96b6963b8bbe35df0b0286846caa8912ebf7f
waleed <waleed.sukhon77@gmail.com>
2021-04-18 19:40:21 +0300

    teacher application

--- Resolvers/types/TeacherApplication.js
+++ Resolvers/types/TeacherApplication.js
@@ -1,6 +1,6 @@
 const TeacherApplication ={
     schoolTypeName(parent,args,{req}){
-        if(parent.educationLevel)
+        if(parent.educationLevel&&parent.educationLevel[0])
             return parent.educationLevel[0].type.name[req.headers.lang||"eng"]
     }
 }
