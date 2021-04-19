2930377f9cbc80994dde152e2e825ec237ecb5d7
waleed <waleed.sukhon77@gmail.com>
2021-04-20 01:30:00 +0300

    g

--- Resolvers/query/ADMIN/getTeacherPayments.js
+++ Resolvers/query/ADMIN/getTeacherPayments.js
@@ -1,4 +1,5 @@
 import {checkToken} from './../../../methods/Tokens.js'
+import { v4 as uuidv4 } from 'uuid';
 export default async(parent,{teacherId},{prisma,req})=>{
     checkToken({token:req.headers.token,Roles:["ADMIN"]})
     return await prisma.appointment.findMany({
@@ -18,7 +19,7 @@ export default async(parent,{teacherId},{prisma,req})=>{
             teacherId,
             payments:e.map((e,index)=>{
                     return{
-                        id:e.payment?e.payment.id:index,
+                        id:e.payment?e.payment.id:uuidv4(),
                         Appointment:e,
                         createdAt:e.payment?e.payment.createdAt:"until now",
                         hasBeenPaid:e.payment?true:false
