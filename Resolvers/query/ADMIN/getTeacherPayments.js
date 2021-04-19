b31554c1cea9edff1007ac8695e706573b7c587b
waleed <waleed.sukhon77@gmail.com>
2021-04-20 01:23:56 +0300

    index

--- Resolvers/query/ADMIN/getTeacherPayments.js
+++ Resolvers/query/ADMIN/getTeacherPayments.js
@@ -16,9 +16,9 @@ export default async(parent,{teacherId},{prisma,req})=>{
     }).then((e)=>{
         return {
             teacherId,
-            payments:e.map((e)=>{
+            payments:e.map((e,index)=>{
                     return{
-                        id:e.payment?e.payment.id:"not paid yet",
+                        id:e.payment?e.payment.id:index,
                         Appointment:e,
                         createdAt:e.payment?e.payment.createdAt:"until now",
                         hasBeenPaid:e.payment?true:false
